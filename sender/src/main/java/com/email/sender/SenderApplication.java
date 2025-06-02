bfd46d27e85b01810bcdb4df003823536155e3b2
Maki Giuseppe de la Vega Silva <pankthera@gmail.com>
2025-06-02 00:51:04 -0500

    refactor: remove dotenv integration and update email properties to use environment variables

--- sender/src/main/java/com/email/sender/SenderApplication.java
+++ sender/src/main/java/com/email/sender/SenderApplication.java
@@ -3,23 +3,14 @@ package com.email.sender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 
 @SpringBootApplication
 public class SenderApplication {
 
 	 public static void main(String[] args) {
-        // --- INICIA CÓDIGO PARA CARGAR .env ---
-        // Carga el archivo .env desde la raíz del proyecto
-        Dotenv dotenv = Dotenv.configure().load();
-
-        // Itera sobre las entradas del .env y las establece como propiedades del sistema
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
-        // --- TERMINA CÓDIGO PARA CARGAR .env ---
-
+       
         SpringApplication.run(SenderApplication.class, args);
     }
 
